d4a8c725313e2032cdd881f11ebbca555f461cd9
F0xedb <tom@odex.be>
2019-10-03 15:24:30 +0200

    Added new development configurations

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-server = "mqtt.farmlab.team"  # The server to connect to
+server = "mira.systems"  # The server to connect to
 user = "Farm"  # the user to connect with mqtt
-passwd = "Lab"  # the password of said user
-port = 1883  # the mqtt port
+passwd = "Lab" # the password of said user
+port = 1887    # the mqtt port
 interval = 10  # how quickly to cycle through mqtt cycles
 food = 22  # pin on the rpi for the food pump
 pump = 27  # pin on the rpi for the water pump
 light = 17  # pin on the rpi for the lights
 sensorfile = "sensors.data"
 
-subscribe = ["/actuator/lightint", "/actuator/flowpump", "/actuator/foodpump"]
+subscribe = ["/sub/actuator/lightint", "/sub/actuator/flowpump", "/sub/actuator/foodpump"]
