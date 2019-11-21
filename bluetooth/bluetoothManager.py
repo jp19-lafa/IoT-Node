1d84894ee4aa8d5e052ead3c0d23f6ca7215e06f
F0xedb <tom@odex.be>
2019-11-21 21:19:32 +0100

    Added comments to clarify certain issues

--- bluetooth/bluetoothManager.py
+++ bluetooth/bluetoothManager.py
@@ -1,9 +1,32 @@
+
+# MIT License
+# 
+# Copyright (c) 2019 AP Hogeschool
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import logger
 import bluetooth
 import subprocess
 import time
 import wifi
 import autopair as pairable
+import config
 
 from bluetooth.ble import DiscoveryService
 
@@ -15,6 +38,10 @@ server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
 
 def pair():
+    """
+    Function to try to pair with a phone.
+    Note that this function accepts all pair request that come in
+    """
     logger.log("Trying to pair with a device", logger.LOG_DEBUG)
     devices = subprocess.check_output("bluetoothctl paired-devices", shell=True)
     # only continue if there is no network
@@ -47,6 +74,9 @@ def pair():
 
 
 def startup(server):
+    """
+    Initialize a bluetooth server so that we can communicate with the client phone
+    """
     logger.log("Starting up the bluetooth module", logger.LOG_DEBUG)
     logger.log("Connected to bluetooth device: {} ".format(pair()), logger.LOG_DEBUG)
     if wifi.ConnectedToTheNetwork():
@@ -56,6 +86,9 @@ def startup(server):
     return server
 
 def idle(server):
+    """
+    Wait until a bluetooth connection is made
+    """
     logger.log("Waiting for a bluetooth connection", logger.LOG_DEBUG)
     if wifi.ConnectedToTheNetwork():
         return None, None
@@ -69,22 +102,30 @@ def idle(server):
     return client, clientInfo
 
 def getWifiData(client, clientInfo, server):
+    """
+    Comminicate with the phone over bluetooth to gather the wifi data here
+    """
     if wifi.ConnectedToTheNetwork():
         return
     logger.log("Receiving wifi credentials", logger.LOG_DEBUG)
-    try:
-        client, clientInfo = server.accept()
-        while 1:
-            data = client.recv(size)
-            if data:
-                logger.log(data)
-                client.send(data) # Echo back to client
-    except:	
-        print("Closing socket")
-        client.close()
-        server.close()
+
+    while 1:
+        data = client.recv(size)
+        if data:
+            logger.log(data)
+            client.send(data) # Echo back to client
+ 
+
+# TODO: implement this function
+def set_name(name):
+    """
+    We change the bluetooth pretier name here
+    """
+    logger.log("Changing bluetooth name to {}".format(name))
+
 
 def EstablishConnection():
+    set_name(config.BLUETOOTH_NAME) # TODO: make a unique id as the farm name
     startup(server)
     client, info = idle(server)
     getWifiData(client, info, server)
