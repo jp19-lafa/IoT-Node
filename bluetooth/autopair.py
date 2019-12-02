cc69a43834b6998c45277eae1af2875c3cdb9882
Restyled.io <commits@restyled.io>
2019-12-02 10:14:15 +0000

    Restyled by autopep8

--- bluetooth/autopair.py
+++ bluetooth/autopair.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
 # MIT License
-# 
+#
 # Copyright (c) 2019 AP Hogeschool
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,45 +28,47 @@ import time
 import pexpect
 import subprocess
 
+
 class BtAutoPair:
-	"""Class to auto pair and trust with bluetooth."""
+    """Class to auto pair and trust with bluetooth."""
 
-	def __init__(self):
-		p = subprocess.Popen("./agent.py", shell = False)
-		out = subprocess.check_output("/usr/sbin/rfkill unblock bluetooth", shell = True)
-		self.child = pexpect.spawn("bluetoothctl", echo = False)
+    def __init__(self):
+        p = subprocess.Popen("./agent.py", shell=False)
+        out = subprocess.check_output(
+            "/usr/sbin/rfkill unblock bluetooth", shell=True)
+        self.child = pexpect.spawn("bluetoothctl", echo=False)
 
-	def get_output(self,command, pause = 0):
-		"""Run a command in bluetoothctl prompt, return output as a list of lines."""
-		self.child.send(command + "\n")
-		time.sleep(pause)
-		start_failed = self.child.expect(["bluetooth", pexpect.EOF])
+    def get_output(self, command, pause=0):
+        """Run a command in bluetoothctl prompt, return output as a list of lines."""
+        self.child.send(command + "\n")
+        time.sleep(pause)
+        start_failed = self.child.expect(["bluetooth", pexpect.EOF])
 
-		if start_failed:
-			raise BluetoothctlError("Bluetoothctl failed after running " + command)
-			
-		return self.child.before.split("\r\n")
+        if start_failed:
+            raise BluetoothctlError(
+                "Bluetoothctl failed after running " + command)
 
-	def enable_pairing(self):
-		"""Make device visible to scanning and enable pairing."""
-		print("pairing enabled")
-		try:
-			out = self.get_output("power on")
-			#out = self.get_output("discoverable-timeout 86400")
-			out = self.get_output("discoverable on")
-			out = self.get_output("pairable on")
-			out = self.get_output("agent off")
+        return self.child.before.split("\r\n")
 
-		except:
-			return None
+    def enable_pairing(self):
+        """Make device visible to scanning and enable pairing."""
+        print("pairing enabled")
+        try:
+            out = self.get_output("power on")
+            #out = self.get_output("discoverable-timeout 86400")
+            out = self.get_output("discoverable on")
+            out = self.get_output("pairable on")
+            out = self.get_output("agent off")
 
-	def disable_pairing(self):
-		"""Disable devices visibility and ability to pair."""
-		try:
-			out = self.get_output("discoverable off")
-			out = self.get_output("pairable off")
+        except:
+            return None
 
-		except e:
-			print(e)
-			return None
+    def disable_pairing(self):
+        """Disable devices visibility and ability to pair."""
+        try:
+            out = self.get_output("discoverable off")
+            out = self.get_output("pairable off")
 
+        except e:
+            print(e)
+            return None
